53e98802419365542efbeade529d4501b8ecce63
Jose Jimeno <jose.jimeno@nextinit.com>
2015-10-28 13:44:06 +0100

    fix call to execCommand

--- richeditor/src/main/assets/rich_editor.js
+++ richeditor/src/main/assets/rich_editor.js
@@ -278,7 +278,7 @@ RE.blurFocus = function() {
 }
 
 RE.removeFormat = function() {
-    execCommand('removeFormat', false, null);
+    document.execCommand('removeFormat', false, null);
 }
 
 // Event Listeners
